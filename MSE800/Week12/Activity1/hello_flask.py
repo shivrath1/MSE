e8765d7ac8f06d24ff32706ce607c2b2ac89546f
shivrath1 <zoomacc07@gmail.com>
2026-06-27 09:59:58 +1200

    MSE800 Week12 Activity1 Update HTML tags

--- MSE800/Week12/Activity1/hello_flask.py
+++ MSE800/Week12/Activity1/hello_flask.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "<p>Hello Flask</p>"
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello Flask Page</title>
+    </head>
+    <body>
+        <p>Hello Flask</p>
+    </body>
+    </html>
+    """
 
 @app.route("/bye")
 def bye():
@@ -12,7 +23,16 @@ def bye():
 
 @app.route("/username/<name>")
 def greet(name):
-    return f"Hello {name}, is learing Flask!"
+    return f"{name}, is learing Flask!"
+
+@app.route("/<name>/<int:number>")
+def learn(name, number):
+    return f"<p>{name}, is learing Flask! {number}.</p>"
+
+@app.route("/resources")
+def resource():
+    return f"<p>Flask Resource <a href='https://flask.palletsprojects.com/en/stable/quickstart/#'>www.flask.palletsprojects.com</a></p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
