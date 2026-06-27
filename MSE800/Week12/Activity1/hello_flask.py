8c14bc61c56ce2385e9ae3c58e472b9ba0188ad6
shivrath1 <zoomacc07@gmail.com>
2026-06-27 22:36:39 +1200

    MSE800 Week12 Activity Display Image from Local Storage

--- MSE800/Week12/Activity1/hello_flask.py
+++ MSE800/Week12/Activity1/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,17 @@ def hello_flask():
     <head>
         <meta charset="UTF-8">
         <title>Hello Flask Page</title>
+        <style>
+            p {
+                color: green;
+            }
+            h1 {
+                color: red;
+            }
+            html {
+                background: cyan;
+            }
+        </style>
     </head>
     <body>
         <p>Hello Flask</p>
@@ -33,6 +44,14 @@ def learn(name, number):
 def resource():
     return f"<p>Flask Resource <a href='https://flask.palletsprojects.com/en/stable/quickstart/#'>www.flask.palletsprojects.com</a></p>"
 
+@app.route("/view")
+def display_image_from_local():
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head><title>Image Display</title></head>
+    <body><h1>Here's your image:</h1><img src="{url_for('static', filename='auckland.jpg')}" alt="My Local Image"></body>
+    </html>"""
 
 if __name__ == "__main__":
     app.run(debug=True)
